18ecd5b3d40330d37259d1c32bcb1a28a76f5b42
vcoder-1 <reddychallur45@gmail.com>
2025-08-14 19:14:39 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,42 @@
+print("welcome to my compter quiz!")
+
+playing = input("Do you went to play? ")
+
+if playing.lower() !="yes":
+    quit()
+
+print("okay! let's play :)")
+score = 0
+
+answer = input("what does CPU stad for?")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("what does GPU stad for?")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("what does Ram stand for?")
+if answer.lower()== "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("what does psu stand for?")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("YOU got " + str(score) + "qustion correct!")
+
+print("YOU got " + str((score / 4) * 100) + "%")
+
